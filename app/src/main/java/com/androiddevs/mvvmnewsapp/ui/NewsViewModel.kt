5487cf0f63c51438c8db5b011b5dfd5f772754bf
Chan Woo Park <14cpark@gmail.com>
2020-11-02 15:31:35 -0800

    Handle BreakingNewsResponse

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
@@ -1,10 +1,40 @@
 package com.androiddevs.mvvmnewsapp.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androiddevs.mvvmnewsapp.models.NewsResponse
 import com.androiddevs.mvvmnewsapp.repository.NewsRepository
+import com.androiddevs.mvvmnewsapp.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel(
     val newsRepository: NewsRepository
 ) : ViewModel() {
 
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val breakingNewsPage = 1 // pagination must be handled in ViewModel; num would reset if defined in fragment
+
+    init { // load news on init
+        getBreakingNews("us")
+    }
+
+    // viewModelScope: makes sure this coroutine stays alive as long as VM is alive
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        // emit loading state to livedata
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
+    }
+
+    // decide whether to emit success state in breakingNews LiveData or error state
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
