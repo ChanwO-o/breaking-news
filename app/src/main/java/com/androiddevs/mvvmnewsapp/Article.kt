b31e91c222433ff902a21b68f80a6d5463190595
Chan Woo Park <14cpark@gmail.com>
2020-11-01 20:01:23 -0800

    DAO and Article setup Room

--- app/src/main/java/com/androiddevs/mvvmnewsapp/Article.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/Article.kt
@@ -1,6 +1,15 @@
 package com.androiddevs.mvvmnewsapp
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// defines a table row in database
+@Entity(
+    tableName = "articles"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true) // autoGenerate: handles auto increment of id
+    var id: Int? = null, // not every article will have an id, some will be from Retrofit that we don't save into database
     val author: String,
     val content: String,
     val description: String,
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticleDao.kt
@@ -0,0 +1,18 @@
+package com.androiddevs.mvvmnewsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.androiddevs.mvvmnewsapp.Article
+
+@Dao // tell Room that this interface defines Dao functions
+interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE) // onConflict: inserted article already exists. REPLACE: replace existing article
+    suspend fun upsert(article: Article): Long // upsert: update or insert.
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>> // not a suspend function; this one returns LiveData obj
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
