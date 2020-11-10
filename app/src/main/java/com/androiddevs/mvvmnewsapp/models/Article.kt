58d7feabf369568246443d16eaf289126b535b6e
Chan Woo Park <14cpark@gmail.com>
2020-11-10 15:14:01 -0800

    Fix Article properties to allow null values

--- app/src/main/java/com/androiddevs/mvvmnewsapp/models/Article.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/models/Article.kt
@@ -11,12 +11,12 @@ import java.io.Serializable
 data class Article(
     @PrimaryKey(autoGenerate = true) // autoGenerate: handles auto increment of id
     var id: Int? = null, // not every article will have an id, some will be from Retrofit that we don't save into database
-    val author: String,
-    val content: String,
-    val description: String,
-    val publishedAt: String,
-    val source: Source,
-    val title: String,
-    val url: String,
-    val urlToImage: String
+    val author: String?, // same for every other attribute, null-checks
+    val content: String?,
+    val description: String?,
+    val publishedAt: String?,
+    val source: Source?,
+    val title: String?,
+    val url: String?,
+    val urlToImage: String?
 ) : Serializable
